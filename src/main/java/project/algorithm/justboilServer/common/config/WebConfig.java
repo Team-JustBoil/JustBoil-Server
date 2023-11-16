2fcaa3d42b7ac4dddfd863a733ecbba5780319b0
ddongseop <dlehdtjq00o@gmail.com>
2023-11-16 19:34:18 +0900

    [CHORE] CORS 오류 해결

---
+++ src/main/java/project/algorithm/justboilServer/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package project.algorithm.justboilServer.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:3000", "http://localhost:3001")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
